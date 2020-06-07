7c80f86b62d9bd7634df62bf67d0d474f9ffcfc4
rakshitraj <rraj.rakshit@gmail.com>
2020-06-07 15:35:52 +0530

    Euclidean GCD algorithm

--- Algorithmic Toolbox/Algorithmic Warm Up/Greatest Common Divisor/gcd.py
+++ Algorithmic Toolbox/Algorithmic Warm Up/Greatest Common Divisor/gcd.py
@@ -14,8 +14,10 @@ def gcd_naive(a, b):
 def gcd(a, b):
     assert 0 <= a <= 2 * 10 ** 9 and 0 <= b <= 2 * 10 ** 9
 
-    type here
-
+    if b == 0 :
+        return a
+    a_ = a%b
+    return gcd(b, a_)
 
 if __name__ == '__main__':
     input_a, input_b = map(int, input().split())
