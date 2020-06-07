ff61e594dee857a4b7b4f88294aede2cd58926f4
rakshitraj <rraj.rakshit@gmail.com>
2020-06-07 16:08:07 +0530

    CORRECTED: last digit of nth large fibonacci number ----> since the first iteration is already handled during initialization the range function will remove the the last iteration to compensate

--- Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
+++ Algorithmic Toolbox/Algorithmic Warm Up/Last Digit of Fibonacci Number/last_digit_of_fibonacci_number.py
@@ -14,7 +14,9 @@ def last_digit_of_fibonacci_number(n):
     assert 0 <= n <= 10 ** 7
     # Implementing property of Pisano Period
     current, next = 0,1
-    for _ in range(n+1):
+    # since the first iteration is already handled during initialization
+    # the range function will remove the the last iteration to compensate
+    for _ in range(n):
         current, next = next, (current+next)%10
     return current
 
