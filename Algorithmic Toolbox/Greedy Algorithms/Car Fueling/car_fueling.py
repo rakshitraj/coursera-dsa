e5457f6fdc867226fde12ec916deb5890b55ef7c
rakshitraj <rraj.rakshit@gmail.com>
2020-07-01 23:19:08 +0530

    Car Fueling problem, greedy algorithm

--- Algorithmic Toolbox/Greedy Algorithms/Car Fueling/car_fueling.py
+++ Algorithmic Toolbox/Greedy Algorithms/Car Fueling/car_fueling.py
@@ -7,18 +7,20 @@ def compute_min_number_of_refills(d, m, stops):
     assert 1 <= len(stops) <= 300
     assert 0 < stops[0] and all(stops[i] < stops[i + 1] for i in range(len(stops) - 1)) and stops[-1] < d
 
+    stops.insert(0,0)
+    stops.insert((len(stops)),d)
     numRefills = 0
     currentRefill = 0
-    while( currentRefill < stops[-1] ):
+    while (currentRefill < len(stops) ):
         lastRefill = currentRefill
-        while( currentRefill <= stops[-1] and
-               (stops[currentRefill + 1] -stops[lastRefill]) <= m):
-            currentRefill = currentRefill + 1
-            if currentRefill == lastRefill:
-                return -1
-            if currentRefill < len(stops):
-                numRefills = numRefills + 1
-    return numRefills
+        while (currentRefill < len(stops) and
+        stops[currentRefill+1] - stops[lastRefill] <= m):
+            currentRefill = currentRefill+1
+        if currentRefill == lastRefill:
+            return -1
+        if currentRefill < len(stops) :
+            numRefills = numRefills + 1
+        return numRefills
 
 if __name__ == '__main__':
     input_d = int(input())
