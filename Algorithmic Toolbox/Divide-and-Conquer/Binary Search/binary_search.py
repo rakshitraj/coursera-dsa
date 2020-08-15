1c5b5dc05e6d91c984c6cb483d20b22efaa36274
rakshitraj <rraj.rakshit@gmail.com>
2020-08-15 17:45:00 +0530

    reset binary search

--- Algorithmic Toolbox/Divide-and-Conquer/Binary Search/binary_search.py
+++ Algorithmic Toolbox/Divide-and-Conquer/Binary Search/binary_search.py
@@ -13,19 +13,12 @@ def binary_search(keys, query):
     assert all(keys[i] < keys[i + 1] for i in range(len(keys) - 1))
     assert 1 <= len(keys) <= 3 * 10 ** 4
 
-
+    type here
 
 
 if __name__ == '__main__':
     input_keys = list(map(int, input().split()))[1:]
     input_queries = list(map(int, input().split()))[1:]
-    mid: int = int((len(keys)) / 2)
-    if keys[mid] == query:
-        return mid
-    elif keys[mid] > query:
-        return binary_search(keys[:mid], query)
-    else keys[mid] < query:
-        return binary_search(keys[mid + 1:], query)
-    return -1
+
     for q in input_queries:
         print(binary_search(input_keys, q), end=' ')
